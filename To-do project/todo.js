fd60ef953d82f7e6f41ee6379416b70076e94ada
jitendra kumar sarraf <23cs072@kpriet.ac.in>
2024-10-17 02:38:36 +0530

    created a function for display the entered elements

--- To-do project/todo.js
+++ To-do project/todo.js
@@ -1,5 +1,28 @@
+let todoList = [];
+
+
 function addTodo(){
   let inputElement=document.querySelector('#todo-input');
   let todoItem=inputElement.value;
   console.log(todoItem);
+  todoList.push(todoItem);
+  inputElement.value='';
+  displayItems();
+}
+
+function displayItems(){
+  let containerElement=document.querySelector('.todo-container');
+  let newHtml='';
+
+  for(let i=0; i<todoList.length; i++){
+    newHtml+=`
+    <P>${todoList[i]}</P>
+    <button>Delete</button>
+    `;
+  }
+  containerElement.innerHTML=newHtml;
+
+  // displayElement.innerText='';
+  // for(let i=0; i<todoList.length; i++){
+  //   displayElement.innerText=displayElement.innerText + '\n' + todoList[i];
 }
